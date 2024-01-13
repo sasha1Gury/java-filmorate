72747f80b598e9d3d12c9f3e169e6d4d300b3b1d
sasha1Gury <sashagury456123@mail.ru>
2024-01-13 17:28:33 +0300

    feat: add validation in methods

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,8 +2,11 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NewFilmException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,17 +22,24 @@ public class FilmController {
     }
 
     @PostMapping
-    public void addFilm(@RequestBody Film film) {
-
+    public void addFilm(@Valid @RequestBody Film film) throws ValidationException {
+        if (!isAfter1895(film)) {
+            throw new ValidationException();
+        }
         films.put(film.hashCode(), film);
     }
 
     @PutMapping
-    public void updateFilm(@RequestBody Film film) throws NewFilmException{
+    public void updateFilm(@Valid @RequestBody Film film) throws NewFilmException{
         if(films.containsValue(film)){
             throw new NewFilmException(film.getName());
         }
         films.put(film.hashCode(), film);
+    }
+
+    private boolean isAfter1895(Film film) {
+        LocalDate date = LocalDate.of(1895, 12, 28);
+        return film.getReleaseDate().isAfter(date);
     }
 
 }
