3d9c8e33e6e99e7405e674960acbb142876b654d
sasha1Gury <sashagury456123@mail.ru>
2024-01-16 01:59:58 +0300

    feat: add UserValidationTest

---
+++ src/test/java/ru/yandex/practicum/filmorate/Model/UserValidationTest.java
@@ -0,0 +1,56 @@
+package ru.yandex.practicum.filmorate.Model;
+
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.ConstraintViolation;
+import java.time.LocalDate;
+import java.util.Set;
+
+public class UserValidationTest {
+    private final LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+
+    @BeforeEach
+    public void setUp() {
+        validator.afterPropertiesSet();
+    }
+
+    @Test
+    public void validationTestValidUser() {
+        User validUser = new User(1, "test@example.com", "Login", "John Doe", LocalDate.now());
+
+        Set<ConstraintViolation<User>> violations = validator.validate(validUser);
+        Assertions.assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    public void validationTestInvalidEmail() {
+        User invalidUser = new User(1, "invalidEmail", "Login", "John Doe", LocalDate.now());
+
+        Set<ConstraintViolation<User>> violations = validator.validate(invalidUser);
+        Assertions.assertEquals(1, violations.size());
+        Assertions.assertEquals("email", violations.iterator().next().getPropertyPath().toString());
+    }
+
+    @Test
+    public void validationTestBlankLogin() {
+        User invalidUser = new User(1, "test@example.com", "", "John Doe", LocalDate.now());
+
+        Set<ConstraintViolation<User>> violations = validator.validate(invalidUser);
+        Assertions.assertEquals(1, violations.size());
+        Assertions.assertEquals("login", violations.iterator().next().getPropertyPath().toString());
+    }
+
+    @Test
+    public void validationTestInvalidBirthday() {
+        User invalidUser = new User(1, "test@example.com", "Login", "John Doe",
+                LocalDate.of(2050, 1, 1));
+
+        Set<ConstraintViolation<User>> violations = validator.validate(invalidUser);
+        Assertions.assertEquals(1, violations.size());
+        Assertions.assertEquals("birthday", violations.iterator().next().getPropertyPath().toString());
+    }
+}
