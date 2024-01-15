bbbf871bf5269c120f73d085288b1074d8cbcb7a
sasha1Gury <sashagury456123@mail.ru>
2024-01-16 01:59:34 +0300

    feat: add validation annotation

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,14 +1,23 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
 
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 public class User {
-    private int id;
+    @EqualsAndHashCode.Exclude private int id;
+    @Email
     private String email;
+    @NonNull
+    @NotBlank
     private String login;
     private String name;
+    @PastOrPresent
     private LocalDate birthday;
 }
