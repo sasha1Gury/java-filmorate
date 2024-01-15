975f5907b005558ea223f2c5bca8fc8f4f6f793f
sasha1Gury <sashagury456123@mail.ru>
2024-01-16 01:57:05 +0300

    fix: add validate in methods

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NewFilmException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
@@ -13,8 +14,11 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
     Map<Integer, Film> films = new HashMap<>();
+    private int idCounter = 1;
 
     @GetMapping
     public List<Film> getAllFilms() {
@@ -22,24 +26,38 @@ public class FilmController {
     }
 
     @PostMapping
-    public void addFilm(@Valid @RequestBody Film film) throws ValidationException {
+    public Film addFilm(@Valid @RequestBody Film film) throws ValidationException, NewFilmException {
         if (!isAfter1895(film)) {
+            log.warn("Фильм + " + film.getName() + " не создан");
             throw new ValidationException();
         }
-        films.put(film.hashCode(), film);
+        if (films.containsValue(film)) {
+            log.warn("Фильм " + film.getName() + " не создан");
+            throw new NewFilmException(film.getName());
+        }
+        film.setId(idCounter++);
+        films.put(film.getId(), film);
+        log.info("Создан фильм " + film.getName());
+        return film;
+
     }
 
     @PutMapping
-    public void updateFilm(@Valid @RequestBody Film film) throws NewFilmException{
+    public Film updateFilm(@Valid @RequestBody Film film) throws ValidationException, NewFilmException{
         if(films.containsValue(film)){
             throw new NewFilmException(film.getName());
         }
-        films.put(film.hashCode(), film);
+        if (!isAfter1895(film)) {
+            log.warn("Фильм + " + film.getName() + " не создан");
+            throw new ValidationException();
+        }
+        log.info("Фильм " + films.get(film.getId()).getName() +  " перезаписан на " + film.getName());
+        films.put(film.getId(), film);
+        return film;
     }
 
     private boolean isAfter1895(Film film) {
         LocalDate date = LocalDate.of(1895, 12, 28);
         return film.getReleaseDate().isAfter(date);
     }
-
 }
