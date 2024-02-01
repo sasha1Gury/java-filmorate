ddb983984e87080e1b760b5527352f443e7b4730
sasha1Gury <sashagury456123@mail.ru>
2024-02-01 21:13:57 +0300

    feat: add friend and get friends

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,4 +1,26 @@
 package ru.yandex.practicum.filmorate.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+@Service
 public class UserService {
+    private final UserStorage userStorage;
+
+    @Autowired
+    public UserService(InMemoryUserStorage userStorage) {
+        this.userStorage = userStorage;
+    }
+
+    public void addFriend(Long id, Long friendId) {
+        userStorage.getUserById(id).addFriend(friendId);
+        userStorage.getUserById(friendId).addFriend(id);
+    }
+
+    public void deleteFriend(Long id, Long friendId) {
+        userStorage.getUserById(id).deleteFriend(friendId);
+        userStorage.getUserById(friendId).deleteFriend(id);
+    }
 }
