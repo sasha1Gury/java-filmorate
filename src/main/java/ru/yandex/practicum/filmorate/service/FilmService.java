7f9c7edacfc8be352754943cceac38cfc655c199
sasha1Gury <sashagury456123@mail.ru>
2024-02-02 14:25:27 +0300

    feat: add DI

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,4 +1,16 @@
 package ru.yandex.practicum.filmorate.service;
 
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+
+@Service
 public class FilmService {
+    private final FilmStorage filmStorage;
+
+    public FilmService(FilmStorage filmStorage) {
+        this.filmStorage = filmStorage;
+    }
+
+
+
 }
