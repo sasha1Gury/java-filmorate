195b589053307aa4a5439961f3d9ff23dba15132
sasha1Gury <sashagury456123@mail.ru>
2024-02-06 16:52:32 +0300

    feat: add get all users

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -2,9 +2,12 @@ package ru.yandex.practicum.filmorate.storage.user;
 
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.util.List;
+
 public interface UserStorage {
     public User addUser(User user);
     public User updateUser(User user);
     public void deleteUser(long id);
     public User getUserById(long id);
+    public List<User> getAllUsers();
 }
