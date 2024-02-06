123f9d7792eca0524d872d9090994a62281954dc
sasha1Gury <sashagury456123@mail.ru>
2024-02-06 16:52:56 +0300

    feat: add string constructor

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,6 +1,10 @@
 package ru.yandex.practicum.filmorate.exception;
 
 public class ValidationException extends RuntimeException {
+    public ValidationException(String s) {
+        super(s);
+    }
+
     public ValidationException() {
         super("Ошибка валидации");
     }
