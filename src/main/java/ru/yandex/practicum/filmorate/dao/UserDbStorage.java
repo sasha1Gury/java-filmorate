0246d0c47608855482ce6ba47e02e351fcc181b5
sasha1Gury <sashagury456123@mail.ru>
2024-02-23 21:26:55 +0300

    fix: fix all methods

--- src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
@@ -1,17 +1,21 @@
 package ru.yandex.practicum.filmorate.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.HashSet;
+import java.sql.*;
+import java.time.LocalDate;
 import java.util.List;
 
 @Component
+@Qualifier("userDbStorage")
 public class UserDbStorage implements UserStorage {
 
     private final JdbcTemplate jdbcTemplate;
@@ -23,26 +27,47 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public User addUser(User user) {
-        String sqlQuery = "INSERT INTO Users(user_id, email, login, name, birthday) VALUES (?, ?, ?, ?, ?)";
-        jdbcTemplate.update(sqlQuery,
-                user.getId(), user.getEmail(), user.getLogin(), user.getName(), user.getBirthday());
+        String sqlQuery = "INSERT INTO \"Users\" (\"email\", \"login\", \"name\", \"birthday\") VALUES (?, ?, ?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement stmt = connection.prepareStatement(sqlQuery, Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, user.getEmail());
+            stmt.setString(2, user.getLogin());
+            stmt.setString(3, user.getName());
+            final LocalDate birthday = user.getBirthday();
+            if (birthday == null) {
+                stmt.setNull(4, Types.DATE);
+            } else {
+                stmt.setDate(4, Date.valueOf(birthday));
+            }
+            return stmt;
+        }, keyHolder);
+
+        user.setId(keyHolder.getKey().longValue());
 
         return user;
     }
 
     @Override
     public User updateUser(User user) {
-        String sqlQuery = "UPDATE Users set user_id = ?, email = ?, login = ?, name = ?, birthday = ? WHERE user_id = ?";
+        String sqlQuery = "UPDATE \"Users\" SET \"email\" = ?, " +
+                "\"login\" = ?, \"name\" = ?, \"birthday\" = ? " +
+                "WHERE \"user_id\" = ?;";
 
-        jdbcTemplate.update(sqlQuery,
-                user.getId(), user.getEmail(), user.getLogin(), user.getName(), user.getBirthday());
+        int rowsAffected = jdbcTemplate.update(sqlQuery,
+                user.getEmail(), user.getLogin(), user.getName(), Date.valueOf(user.getBirthday()), user.getId());
+
+        if (rowsAffected < 1) {
+            throw new NotFoundException(user.getId() + " не найден");
+        }
 
         return user;
     }
 
     @Override
     public void deleteUser(long id) {
-        String sqlQuery = "DELETE FROM Users where user_id = ?";
+        String sqlQuery = "DELETE FROM \"Users\" where user_id = ?";
 
         jdbcTemplate.update(sqlQuery, id);
 
@@ -50,21 +75,21 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public User getUserById(long id) {
-        String sqlQuery = "SELECT * FROM Users WHERE user_id = ?";
+        String sqlQuery = "SELECT * FROM \"Users\" WHERE user_id = ?";
 
         return jdbcTemplate.queryForObject(sqlQuery, this::mapRowToUser, id);
     }
 
     @Override
     public List<User> getAllUsers() {
-        String sqlQuery = "Select * from Users";
+        String sqlQuery = "Select * from \"Users\"";
 
         return jdbcTemplate.query(sqlQuery, this::mapRowToUser);
     }
 
-    @Override
+    //@Override
     public List<User> getUserFriends(User user) {
-        String sqlQuery = "Select * from Users u " +
+        String sqlQuery = "Select * from \"Users\" u " +
                 "LEFT JOIN friends f on u.user_id=f.user1_id" +
                 "WHERE user_id = ?";
 
@@ -77,8 +102,7 @@ public class UserDbStorage implements UserStorage {
                 resultSet.getString("email"),
                 resultSet.getString("login"),
                 resultSet.getString("name"),
-                resultSet.getDate("birthday").toLocalDate(),
-                new HashSet<>()
+                resultSet.getDate("birthday").toLocalDate()
         );
     }
 }
