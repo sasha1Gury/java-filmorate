c5f4c8882eebb77ea75cb5423c33adf02abf0415
sasha1Gury <sashagury456123@mail.ru>
2024-02-24 14:39:40 +0300

    feat: add enum genre

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.filmorate.model;
+
+public enum Genre {
+    ACTION,
+    ADVENTURE,
+    COMEDY,
+    DRAMA,
+    CRIME,
+    HORROR,
+    FANTASY,
+    ROMANCE,
+    THRILLER,
+    ANIMATION,
+    FAMILY,
+    WAR,
+    DOCUMENTARY,
+    MUSIC,
+    BIOGRAPHY,
+    SCI_FI,
+    WESTERN,
+    POST_APOCALYPTIC
+}
