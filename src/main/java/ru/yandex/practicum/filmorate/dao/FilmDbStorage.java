50ba5316c13664a0da0793323ebf94cdf8dda169
sasha1Gury <sashagury456123@mail.ru>
2024-02-24 14:39:28 +0300

    feat: add FilmDbStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
@@ -0,0 +1,47 @@
+package ru.yandex.practicum.filmorate.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+
+import java.util.List;
+
+@Component
+@Qualifier("FilmDbStorage")
+public class FilmDbStorage implements FilmStorage {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public FilmDbStorage(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Film addFilm(Film film) {
+        return null;
+    }
+
+    @Override
+    public Film updateFilm(Film film) {
+        return null;
+    }
+
+    @Override
+    public void deleteFilm(long id) {
+
+    }
+
+    @Override
+    public List<Film> getAllFilms() {
+        return null;
+    }
+
+    @Override
+    public Film getFilmById(Long id) {
+        return null;
+    }
+}
