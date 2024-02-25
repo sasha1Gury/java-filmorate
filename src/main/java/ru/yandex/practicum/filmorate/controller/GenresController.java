33096b06c418d840328ced27047be65ea024ad48
sasha1Gury <sashagury456123@mail.ru>
2024-02-25 19:22:55 +0300

    feat: add logic for methods

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenresController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenresController.java
@@ -1,6 +1,34 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.GenresDbService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/genres")
+@Slf4j
 public class GenresController {
 
+    private final GenresDbService genresDbService;
+
+    public GenresController(GenresDbService genresDbService) {
+        this.genresDbService = genresDbService;
+    }
+
+    @GetMapping
+    public List<Film.Genre> getAllGenres() {
+        return genresDbService.getAllGenres();
+    }
+
+    @GetMapping("/{id}")
+    public Film.Genre getGenreById(@PathVariable("id") long id) {
+        return genresDbService.getGenreById(id);
+    }
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/dao/GenresDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/GenresDbStorage.java
@@ -1,4 +1,39 @@
 package ru.yandex.practicum.filmorate.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.GenreEnum;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Component
 public class GenresDbStorage {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public GenresDbStorage(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Film.Genre> getAllGenres() {
+        String sqlQuery = "SELECT * FROM \"Genre\" g ";
+
+        return jdbcTemplate.query(sqlQuery, this::mapRowToGenres);
+    }
+
+    public Film.Genre getGenreById(long id) {
+        String sqlQuery = "SELECT * FROM \"Genre\" g WHERE g.\"genre_id\" = ?; ";
+
+        return jdbcTemplate.queryForObject(sqlQuery, this::mapRowToGenres, id);
+    }
+
+    private Film.Genre mapRowToGenres(ResultSet resultSet, int rowNum) throws SQLException {
+        Film.Genre obj = new Film.Genre();
+        obj.setId(resultSet.getInt("genre_id"));
+        obj.setName(GenreEnum.valueOfDisplayName(resultSet.getString("name")).toString());
+        return obj;
+    }
 }
